32bbabe75ad9cf89a541a7b27ba6680396571a08
Sanghyeon Kim <tkdgus96@gmail.com>
2024-01-26 17:59:38 +0900

    fix: str인 부분을 Number로 변경 후 연산하도록 유도

--- src/components/ItemTooltip.tsx
+++ src/components/ItemTooltip.tsx
@@ -66,56 +66,56 @@ const ItemBox = ({ item }: { item: ItemEquipment }) => {
       />
       <div>
         <div>장비 분류: {item.item_equipment_slot}</div>
-        {!!item.item_total_option.str && (
+        {!!Number(item.item_total_option.str) && (
           <div>STR : +{item.item_total_option.str}</div>
         )}
-        {!!item.item_total_option.dex && (
+        {!!Number(item.item_total_option.dex) && (
           <div>DEX : +{item.item_total_option.dex}</div>
         )}
-        {!!item.item_total_option.int && (
+        {!!Number(item.item_total_option.int) && (
           <div>INT : +{item.item_total_option.int}</div>
         )}
-        {!!item.item_total_option.luk && (
+        {!!Number(item.item_total_option.luk) && (
           <div>LUK : +{item.item_total_option.luk}</div>
         )}
-        {!!item.item_total_option.max_hp && (
+        {!!Number(item.item_total_option.max_hp) && (
           <div>최대 HP : +{item.item_total_option.max_hp}</div>
         )}
-        {!!item.item_total_option.max_hp_rate && (
+        {!!Number(item.item_total_option.max_hp_rate) && (
           <div>최대 HP : +{item.item_total_option.max_hp}</div>
         )}
-        {!!item.item_total_option.max_mp && (
+        {!!Number(item.item_total_option.max_mp) && (
           <div>최대 MP : +{item.item_total_option.max_mp}</div>
         )}
-        {!!item.item_total_option.max_hp_rate && (
+        {!!Number(item.item_total_option.max_hp_rate) && (
           <div>최대 MP : +{item.item_total_option.max_mp}</div>
         )}
-        {!!item.item_total_option.attack_power && (
+        {!!Number(item.item_total_option.attack_power) && (
           <div>공격력 : +{item.item_total_option.attack_power}</div>
         )}
-        {!!item.item_total_option.boss_damage && (
+        {!!Number(item.item_total_option.boss_damage) && (
           <div>
             보스 몬스터 공격 시 데미지 : +{item.item_total_option.boss_damage}%
           </div>
         )}
-        {!!item.item_total_option.ignore_monster_armor && (
+        {!!Number(item.item_total_option.ignore_monster_armor) && (
           <div>
             몬스터 방어율 무시 : +{item.item_total_option.ignore_monster_armor}%
           </div>
         )}
-        {!!item.item_total_option.damage && (
+        {!!Number(item.item_total_option.damage) && (
           <div>데미지 : +{item.item_total_option.damage}</div>
         )}
-        {!!item.item_total_option.all_stat && (
+        {!!Number(item.item_total_option.all_stat) && (
           <div>올스탯 : +{item.item_total_option.all_stat}%</div>
         )}
-        {!!item.item_total_option.speed && (
+        {!!Number(item.item_total_option.speed) && (
           <div>이동속도 : +{item.item_total_option.speed}</div>
         )}
-        {!!item.item_total_option.jump && (
+        {!!Number(item.item_total_option.jump) && (
           <div>점프력 : +{item.item_total_option.jump}</div>
         )}
-        {!!item.item_total_option.equipment_level_decrease && (
+        {!!Number(item.item_total_option.equipment_level_decrease) && (
           <div>
             착용 레벨 감소 : +{item.item_total_option.equipment_level_decrease}
           </div>
