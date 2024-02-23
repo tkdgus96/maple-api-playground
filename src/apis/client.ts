704ff10b74d23d5d679197b7e8883baf9df45650
tkdgus96 <tkdgus96@gmail.com>
2024-02-23 16:05:47 +0900

    env: 배포환경변수 설정

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -8,7 +8,9 @@ const client = axios.create({
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "x-nxopen-api-key":
-      "live_760dd7575f03db5bdda1171af9fd554ae18d2e3d35959fb50467974b08ac7625ffaa9978cb4c6a7d9fab4c6f9fcb8bc4",
+      import.meta.env.MODE === "development"
+        ? import.meta.env.VITE_API_KEY
+        : process.env.API_KEY,
   },
   responseType: "json",
 });
